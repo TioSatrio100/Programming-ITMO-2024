b0c73933fb0f8612bcc9050480efe2db2fa29829
TioSatrio100 <brahmantoro.subagio@gmail.com>
2024-10-11 00:50:11 +0300

    Реализована функция encrypt_vigenre()

--- src/lab2/vigenre.py
+++ src/lab2/vigenre.py
@@ -8,8 +8,29 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    ciphertext = ""
+
     # PUT YOUR CODE HERE
+    ciphertext = ""
+    keyword_repeated = ""
+    keyword_length = len(keyword)
+
+    keyword_index = 0
+    for char in plaintext:
+        if char.isalpha():
+            keyword_repeated += keyword[keyword_index % keyword_length].upper()
+            keyword_index += 1
+        else:
+            keyword_repeated += char
+
+    for p_char, k_char in zip(plaintext, keyword_repeated):
+        if p_char.isalpha():
+            shift = ord(k_char) - ord('A')
+            base = ord('A') if p_char.isupper() else ord('a')
+            new_char = chr((ord(p_char) - base + shift) % 26 + base)
+            ciphertext += new_char
+        else:
+            ciphertext += p_char
+
     return ciphertext
 
 
@@ -25,4 +46,5 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     plaintext = ""
     # PUT YOUR CODE HERE
+
     return plaintext
