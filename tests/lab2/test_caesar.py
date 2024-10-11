d2e95f9e7655408b408951f2a68c48169f66e64c
Satrio Brahmantoro Adi Subagio <88955341+TioSatrio100@users.noreply.github.com>
2024-10-12 02:39:24 +0300

    solve problem

--- tests/lab2/test_caesar.py
+++ tests/lab2/test_caesar.py
@@ -2,31 +2,5 @@ import unittest
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
-
-import lab2.caesar
-
-class TestCaesarCipher(unittest.TestCase):
-
-    def test_encrypt(self):
-        self.assertEqual(lab2.caesar.encrypt_caesar("PYTHON"), "SBWKRQ")
-        self.assertEqual(lab2.caesar.encrypt_caesar("python"), "sbwkrq")
-        self.assertEqual(lab2.caesar.encrypt_caesar("Python3.6"), "Sbwkrq3.6")
-        self.assertEqual(lab2.caesar.encrypt_caesar(""), "")
-
-    def test_decrypt(self):
-        self.assertEqual(lab2.caesar.decrypt_caesar("SBWKRQ"), "PYTHON")
-        self.assertEqual(lab2.caesar.decrypt_caesar("sbwkrq"), "python")
-        self.assertEqual(lab2.caesar.decrypt_caesar("Sbwkrq3.6"), "Python3.6")
-        self.assertEqual(lab2.caesar.decrypt_caesar(""), "")
-
-    def test_custom_shift(self):
-        self.assertEqual(lab2.caesar.encrypt_caesar("ABC", shift=1), "BCD")
-        self.assertEqual(lab2.caesar.decrypt_caesar("BCD", shift=1), "ABC")
-        self.assertEqual(lab2.caesar.encrypt_caesar("XYZ", shift=3), "ABC")
-        self.assertEqual(lab2.caesar.decrypt_caesar("ABC", shift=3), "XYZ")
-
-if __name__ == "__main__":
-    unittest.main()
 
 
