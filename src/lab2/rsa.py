afa6ef13bc07d1ac8d7016884773f02c97a6a2e0
TioSatrio100 <brahmantoro.subagio@gmail.com>
2024-10-12 02:23:57 +0300

    Реализованы функции multiplicative_inverse() и generate_keypair()

--- src/lab2/rsa.py
+++ src/lab2/rsa.py
@@ -31,3 +31,73 @@ def gcd(a: int, b: int) -> int:
         return gcd(a - b, b)
     return gcd(a, b - a)
 
+def multiplicative_inverse(e: int, phi: int) -> int:
+    """
+    >>> multiplicative_inverse(7, 40)
+    23
+    """
+    # PUT YOUR CODE HERE
+    pass
+    original_phi = phi
+    x0, x1 = 0, 1
+    if phi == 1:
+        return 0
+    while e > 1:
+        q = e // phi
+        e, phi = phi, e % phi
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0:
+        x1 += original_phi
+    return x1
+
+def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
+    if not (is_prime(p) and is_prime(q)):
+        raise ValueError("Both numbers must be prime.")
+    elif p == q:
+        raise ValueError("p and q cannot be equal")
+    # n = pq
+    # PUT YOUR CODE HERE
+    n = p * q
+
+    # phi = (p-1)(q-1)
+    # PUT YOUR CODE HERE
+    phi = (p - 1) * (q - 1)
+
+    # Choose an integer e such that e and phi(n) are coprime
+    e = random.randrange(1, phi)
+
+    # Use Euclid's Algorithm to verify that e and phi(n) are comprime
+    g = gcd(e, phi)
+    while g != 1:
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
+    # Use Extended Euclid's Algorithm to generate the private key
+    d = multiplicative_inverse(e, phi)
+    # Return public and private keypair
+    # Public key is (e, n) and private key is (d, n)
+    return ((e, n), (d, n))
+
+def encrypt(pk: tp.Tuple[int, int], plaintext: str) -> tp.List[int]:
+    key, n = pk
+    cipher = [pow(ord(char), key, n) for char in plaintext]
+    return cipher
+
+def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
+    key, n = pk
+    plain = [chr(pow(char, key, n)) for char in ciphertext]
+    return "".join(plain)
+
+if __name__ == "__main__":
+    print("RSA Encrypter/Decrypter")
+    p = int(input("Enter a prime number (17, 19, 23, etc): "))
+    q = int(input("Enter another prime number (Not one you entered above): "))
+    print("Generating your public/private key pairs now . . .")
+    public, private = generate_keypair(p, q)
+    print("Your public key is ", public, " and your private key is ", private)
+    message = input("Enter a message to encrypt with your private key: ")
+    encrypted_msg = encrypt(private, message)
+    print("Your encrypted message is: ")
+    print(" ".join(map(str, encrypted_msg)))  # Print as space-separated values for clarity
+    print("Decrypting message with public key ", public, " . . .")
+    print("Your message is:")
+    print(decrypt(public, encrypted_msg))
